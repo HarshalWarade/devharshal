6a075b47b61807cd16936455f9c218435be9e2e9
harshalDev <harshalwarade344@gmail.com>
2024-06-24 19:44:42 +0530

    add code

--- func.js
+++ func.js
@@ -1,3 +1,12 @@
 const requestConnection = () => {
+    window.location.href = "more.html"
+}
+
+const teleportLinkedIn = () => {
     window.location.href = "https://www.linkedin.com/in/harshal-warade-31615b236/"
 }
+
+const teleportFiverr = () => {
+    window.location.href = "https://www.fiverr.com/harshaldwarade"
+}
+
